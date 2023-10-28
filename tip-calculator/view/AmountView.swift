433274c3e7f749f70cf4b88a80ea39f544980409
CSPrasad <csprasad195@gmail.com>
2023-10-28 13:30:40 +0530

    more updates on ui

---
+++ tip-calculator/view/AmountView.swift
@@ -0,0 +1,64 @@
+//
+//  AmountView.swift
+//  tip-calculator
+//
+//  Created by CS Prasad on 28/10/23.
+//
+
+import UIKit
+
+class AmountView: UIView {
+    
+    private let title: String
+    private let textAlignment: NSTextAlignment
+    
+    private lazy var namelabel: UILabel = {
+        LabelFactory.build(
+            text: title,
+            font: ThemeFont.regular(ofSize: 16),
+            textColor: ThemeColor.text,
+            textAlignment: textAlignment)
+    }()
+    
+    private lazy var amountLabel: UILabel = {
+       let label = UILabel()
+        label.textAlignment = self.textAlignment
+        label.textColor = ThemeColor.primary
+        let text = NSMutableAttributedString(
+        string: "₹0",
+        attributes: [
+            .font: ThemeFont.bold(ofSize: 24)])
+        text.addAttributes([.font: ThemeFont.bold(ofSize: 16)],
+                           range: NSMakeRange(0, 1))
+        label.attributedText = text
+        return label
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+        namelabel,
+        amountLabel
+        ])
+        stackView.axis = .vertical
+       return stackView
+    }()
+    
+    init(title: String, textAlignement: NSTextAlignment) {
+        self.title = title
+        self.textAlignment = textAlignement
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        addSubview(stackView)
+        
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
